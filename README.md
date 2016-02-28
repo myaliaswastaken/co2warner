272d9c0d6deea065b9dbdb1a7cd0a094d1809bff
myaliaswastaken <a.kujath@gmx.de>
2016-02-28 12:50:22 +0100

    Updated readme and added comments to co2Warner.py

--- README.md
+++ README.md
@@ -1,12 +1,40 @@
-Simple python script that uses the netatmo-api-python library to
-visualize the current co2 level using a raspberry pi and some 
-ws2812 pixel strips in colors from green to red.
+My first python script that uses the netatmo-api-python library to
+visualize the current co2 level using a netatmo weather station, a  raspberry pi and some ws281x pixel strips.
 
-The script pulls the current value from the netatmp api (http://dev.netatmo.com),
+The script authenticates using OAUTH2 using the netatmo-api-python(https://github.com/philippelt/netatmo-api-python), then pulls the current value from the netatmp api (http://dev.netatmo.com),
 converts the current co2 level to a color
 (code inspired by http://stackoverflow.com/questions/20792445/calculate-rgb-value-for-a-range-of-values-to-create-heat-map)
 and uses some ws2812 pixel strips (see https://learn.adafruit.com/neopixels-on-raspberry-pi/overview for info and wiring) 
 to constantly visualize the current level.
+
+If you want to use it, do the following:
+1) Get a raspberry pi
+   I use the version 2 model b and you also need a Netatmo weather station
+
+2) Get some ws281x pixels
+   Mine are http://www.watterott.com/de/WS2812-Stick-10x-RGB-LED
+   But others should work as well, like https://www.adafruit.com/products/1426
+
+3) Wire everything up
+   You can find some help here: https://learn.adafruit.com/neopixels-on-raspberry-pi/wiring
+
+4) Get the netatmo-api-python 
+   from  https://github.com/philippelt/netatmo-api-python
+
+5) Get an Netatmo account and register your application
+   http://dev.netatmo.com/dev/createapp
+
+6) Enter your credentials in the netatmo-api-python library
+   Simply edit lnetatmo.py, set _CLIENT_ID, _CLIENT_ID_SECRET, _USERNAME and _PASSWORD
+
+7) Maybe you want to modify the colors that display the Co2 values
+   then you need to modify CO2_COLOR_GREEN, _YELLOW and _RED in the co2Warner.py
+
+8) Start the script and enjoy
+   You may want to launch it at startup of your pi
+
+
+Please contact me in case you find something that needs to be correct.
 
 
 
--- co2Warner.py
+++ co2Warner.py
@@ -1,20 +1,22 @@
 import lnetatmo, time
 from neopixel import *
 
-DEBUG 		 = True
-LED_COUNT        = 30      # Number of LED pixels.
-LED_PIN          = 18      # GPIO pin connected to the pixels (must support PWM!).
-LED_BRIGHTNESS   = 128	   # Brightness between 0 and 255
-LED_FREQ_HZ      = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA          = 5       # DMA channel to use for generating signal (try 5)
-LED_INVERT       = False   # True to invert the signal (when using NPN transistor level shift)
-CO2_MIN	         = 0
-CO2_MAX	         = 1500
-CO2_STEPS        = 10
-CO2_COLOR_RED    = (128, 0, 0)
-CO2_COLOR_GREEN  = (0, 255, 0)
-CO2_COLOR_YELLOW = (255, 140, 0)
+DEBUG 		 = True	   		# If ture, some debug information will be logged out on the consle
+LED_COUNT        = 30      		# Number of LED pixels.
+LED_PIN          = 18      		# GPIO pin connected to the pixels (must support PWM!).
+LED_BRIGHTNESS   = 128	   		# Brightness between 0 and 255
+LED_FREQ_HZ      = 800000  		# LED signal frequency in hertz (usually 800khz)
+LED_DMA          = 5       		# DMA channel to use for generating signal (try 5)
+LED_INVERT       = False   		# True to invert the signal (when using NPN transistor level shift)
+CO2_MIN	         = 0	   		# Min Co2 value, here the green value starts
+CO2_MAX	         = 1500	   		# Max Co2 value, from here on the red color will be displayed
+CO2_STEPS        = 10			# Steps for color display
+CO2_COLOR_RED    = (128, 0, 0)		# Red color
+CO2_COLOR_GREEN  = (0, 255, 0)		# Green color
+CO2_COLOR_YELLOW = (255, 140, 0)	# Yellow color
 
+# Calculates the color to diplay for current Co2 level
+# See http://stackoverflow.com/questions/20792445/calculate-rgb-value-for-a-range-of-values-to-create-heat-map
 def rgb(minval, maxval, val, colors):
     max_index = len(colors)-1
     v = float(val-minval) / float(maxval-minval) * max_index
@@ -24,8 +26,12 @@ def rgb(minval, maxval, val, colors):
     return int(r1 + f*(r2-r1)), int(g1 + f*(g2-g1)), int(b1 + f*(b2-b1))
 
 
-# Main program logic follows:
+# Main loop
+# Basically inspired by the strandtest example of the ws281x library
+# https://github.com/jgarff/rpi_ws281x/blob/master/python/examples/strandtest.py
 if __name__ == '__main__':
+
+	# Waiting to make sure we have connectivity
 	time.sleep(30)
 
         # Create NeoPixel object with appropriate configuration.
@@ -50,4 +56,5 @@ if __name__ == '__main__':
 		for position in range(0, LED_COUNT):
 			strip.setPixelColorRGB(position, r, g, b)
 		strip.show()
+		# Refresh value every minute
 		time.sleep(60)
